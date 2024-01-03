0ea13a8755cbad419627e852caa5d91ec40c6c05
kimmeh <mhko0411@gmail.com>
2024-01-03 23:14:31 +0900

    feat: TextField 컴포넌트 추가

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,49 @@
+import { type InputHTMLAttributes, forwardRef, ForwardedRef } from "react";
+import styled from "styled-components";
+
+import { colors } from "theme";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: boolean;
+  helperText?: string;
+}
+
+const Box = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const Input = styled.input<{ error: boolean }>`
+  width: 100%;
+  height: 48px;
+  font-size: 1rem;
+  padding: 16px 14px;
+  border-radius: 8px;
+  line-height: 24px;
+  margin: 0;
+  outline: none;
+  border: ${({ error }) => (error ? `1px solid ${colors.error.main}` : 0)};
+  color: ${colors.gray.contrastText};
+  background-color: ${colors.gray.light};
+`;
+
+const HelperText = styled.p`
+  color: ${colors.error.light};
+  font-size: 0.75rem;
+  margin-top: 4;
+`;
+
+const TextField = (
+  { label, error = false, helperText, ...props }: Props,
+  ref: ForwardedRef<HTMLInputElement>
+) => {
+  return (
+    <Box>
+      <Input aria-label={label} ref={ref} error={error} {...props} />
+      {error && helperText && <HelperText>{helperText}</HelperText>}
+    </Box>
+  );
+};
+
+export default forwardRef(TextField);
