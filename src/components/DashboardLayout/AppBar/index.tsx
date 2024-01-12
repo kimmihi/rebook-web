93cf97ce58223063ab8d22721219e89a5e0bc77c
kimmeh <mhko0411@gmail.com>
2024-01-12 21:48:23 +0900

    fix: app bar 수정

--- src/components/DashboardLayout/AppBar/index.tsx
+++ src/components/DashboardLayout/AppBar/index.tsx
@@ -15,22 +15,36 @@ const Header = styled.header`
   top: 0;
   width: 100%;
   height: ${APP_BAR_HEIGHT}px;
-  padding: 32px 16px;
+  padding: 48px 64px;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  box-shadow: 0 1px 7px #e6e6e6;
+  background-color: #fff;
+  z-index: 999;
 `;
 
 const AppBar = () => {
   const navigate = useNavigate();
   const isLoggedIn = accessToken.getToken() === null ? false : true;
 
+  const handleClickLogo = () => {
+    navigate("/");
+  };
+
   const handleClickMyInfo = () => {
     navigate("/me");
   };
   return (
     <Header>
-      <Typography variant="title">Rebook</Typography>
+      <Typography
+        variant="title"
+        role="button"
+        onClick={handleClickLogo}
+        style={{ cursor: "pointer" }}
+      >
+        Rebook
+      </Typography>
       {isLoggedIn && (
         <IconButton onClick={handleClickMyInfo}>
           <FaUserAlt />
